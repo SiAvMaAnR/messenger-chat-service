730a298b104603f22fb6ad548a87f92ca7ef0c3c
Samarkin <siavmaanr@gmail.com>
2024-01-27 21:27:59 +0300

    refactor: remove registration route config

--- Messenger.Domain/Shared/Settings/RoutePathSettings.cs
+++ Messenger.Domain/Shared/Settings/RoutePathSettings.cs
@@ -4,7 +4,6 @@ public class RoutePathSettings
 {
     public const string Path = "RoutePath";
 
-    public string Registration { get; set; } = null!;
     public string Confirmation { get; set; } = null!;
     public string ResetToken { get; set; } = null!;
 }
