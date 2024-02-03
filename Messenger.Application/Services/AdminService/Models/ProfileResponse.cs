6f5342a0e4e83da3ce37a2e8ea1895d205cb78f7
SiAvMaAnR <siavmaanr@gmail.com>
2024-02-03 19:14:30 +0300

    feature: profile for admin

---
+++ Messenger.Application/Services/AdminService/Models/ProfileResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace MessengerX.Application.Services.AdminService.Models;
+
+public class AdminServiceProfileResponse
+{
+    public string Login { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Role { get; set; } = null!;
+}
